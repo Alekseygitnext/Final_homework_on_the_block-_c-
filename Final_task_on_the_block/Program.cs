0739d1c60a0ff7705f7621dfee2a340f93270992
Aleksey Gvozdev <gvozd0227yandex.ru>
2022-11-25 15:20:14 +1000

    добавл программу решения

---
+++ Final_task_on_the_block/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача:
+// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3 символа.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами
+
+string[] array = new string[] { "456", "max", "student", "?3*-/", "min", "mark", "1234" };
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+void FormatArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        string element = arr[i];
+        if (element.Length <= 3)
+        {
+            arr[i] = element;
+    }
+        else arr[i] = string.Empty;
+    }
+}
+PrintArray(array);
+FormatArray(array);
+Console.Write("Условию соответствуют: ");
+PrintArray(array);
